0bca01da9108b1c92d60d723d773e519cf15149a
yeongminkim <oinochoe@naver.com>
2020-08-23 20:58:24 +0900

    레이아웃 수정중

--- codepen/src/components/common/Footer/Footer.js
+++ codepen/src/components/common/Footer/Footer.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import styles from './Footer.scss';
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 const Footer = ({onLoginClick, logged}) => (
   <footer className={cx('footer')}>
-    <Link to="/" className={cx('brand')}>reactblog</Link>
-    <div onClick={onLoginClick} className={cx('admin-login')}>
+    {/* <Link to="/" className={cx('brand')}>PEN</Link> */}
+    {/* <div onClick={onLoginClick} className={cx('admin-login')}>
       { logged ? '로그아웃' : '로그인' }
-    </div>
+    </div> */}
   </footer>
 );
 
