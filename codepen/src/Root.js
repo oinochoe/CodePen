4a3c0c7dc7378a6cf13d2d794509ebc7493c80a1
yeongminkim <oinochoe@naver.com>
2020-08-23 15:50:43 +0900

    client side rendering으로 변경중

--- codepen/src/Root.js
+++ codepen/src/Root.js
@@ -3,20 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import { Provider } from 'react-redux';
 import configure from 'store/configure';
-import transit from 'transit-immutable-js';
 
-const preloadedState = window.__PRELOADED_STATE__ && transit.fromJSON(window.__PRELOADED_STATE__);
-
-const store = configure(preloadedState);
+const store = configure();
 
 const Root = () => {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  );
-}
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    );
+};
 
 export default Root;
