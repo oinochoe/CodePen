d61c4d95efb8c83c0d9a9841df3bbc60d6914cd5
yeongminkim <oinochoe@naver.com>
2020-08-25 01:29:44 +0900

    footer 컴포넌트 수정

--- codepen/src/components/common/Footer/Footer.js
+++ codepen/src/components/common/Footer/Footer.js
@@ -4,13 +4,14 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const Footer = ({onLoginClick, logged}) => (
-  <footer className={cx('footer')}>
-    {/* <Link to="/" className={cx('brand')}>PEN</Link> */}
-    {/* <div onClick={onLoginClick} className={cx('admin-login')}>
+const Footer = ({ onLoginClick, logged }) => (
+    <footer className={cx('footer')}>
+        {/* <Link to="/" className={cx('brand')}>PEN</Link> */}
+        {/* <div onClick={onLoginClick} className={cx('admin-login')}>
       { logged ? '로그아웃' : '로그인' }
     </div> */}
-  </footer>
+        &copy;
+    </footer>
 );
 
 export default Footer;
