311b973f62344441b52b9ea713aa18c172922926
oinochoe <oinochoe@naver.com>
2020-10-13 19:26:50 +0900

    dirtree 사용 예정

--- node.js
+++ node.js
@@ -1,3 +1,14 @@
 const dirTree = require("directory-tree");
-const tree = dirTree("animation");
-console.log(tree.children[0].children);
+const readMarkdown = require('read-markdown')
+const tree = dirTree('./animation', null, (item, PATH, stats) => {
+     console.log(item);
+    // console.log(PATH);
+    //console.log(stats)
+});
+
+
+readMarkdown('./animation/dist/**/*.md')
+    .then(function(data){
+        console.log(data)
+    })
+    .catch(console.error)
