2f126430d5ee1c35900c205eab720135c8445b5d
oinochoe <oinochoe@naver.com>
2020-10-14 19:07:08 +0900

    react tree view 확인중

--- node.js
+++ node.js
@@ -1,14 +1,6 @@
 const dirTree = require("directory-tree");
 const readMarkdown = require('read-markdown')
-const tree = dirTree('./animation', null, (item, PATH, stats) => {
-     console.log(item);
-    // console.log(PATH);
-    //console.log(stats)
+
+dirTree('./animation', {normalizePath:true }, (item, PATH, stats) => {
+    console.log(item);
 });
-
-
-readMarkdown('./animation/dist/**/*.md')
-    .then(function(data){
-        console.log(data)
-    })
-    .catch(console.error)
